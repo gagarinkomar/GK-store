3761b71e44aca45e629c69fdd4c8d4b90190b91c
57412698 <gagarinkomar@users.noreply.github.com>
2020-04-17 20:07:20 +0300

    Исправлены мелкие недочёты.

--- app.py
+++ app.py
@@ -505,7 +505,8 @@ def products_check():
 def main():
     db_session.global_init(os.path.join('db', 'GK-store.sqlite'))
 
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
